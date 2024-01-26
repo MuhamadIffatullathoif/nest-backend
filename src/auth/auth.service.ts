a96fc464733b5e3b6f38095f92a7b95cba35d05a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-26 17:12:12 +0700

    08. Encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./entities/user.entity";
 import { Model } from "mongoose";
 
+import * as bcryptjs from "bcryptjs";
+
 @Injectable()
 export class AuthService {
 
@@ -13,8 +15,15 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
     } catch (error) {
       if (error.code === 11000) {
         throw new BadRequestException(`${createUserDto.email} already exists`);
