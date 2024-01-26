60d42d408a5bac1f2e39abdc6977e300f6819193
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-26 21:23:22 +0700

    13. Validate JWT and get User

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UpdateAuthDto, LoginDto, CreateUserDto, RegisterUserDto } from "./dto";
 import { AuthGuard } from "./guards/auth.guard";
@@ -25,7 +25,8 @@ export class AuthController {
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() req: Request) {
+    const user = req['user'];
     return this.authService.findAll();
   }
 
